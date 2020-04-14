44db50e797a176f4ca3b7a6ad7b9c8164fde8ad3
Serj <sv@spiks.ru>
2020-04-14 10:14:52 +0300

    List courses

--- routes/courses.js
+++ routes/courses.js
@@ -1,11 +1,15 @@
 const {Router} = require('express');
+const Course = require('../models/courses');
 
 const router = Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const courses = await Course.getAll();
+
   res.render('courses', {
     title: 'Курсы',
     isCourses: true,
+    courses,
   });
 });
 
