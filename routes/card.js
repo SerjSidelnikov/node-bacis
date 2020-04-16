1edcc6c9b956082f2712399210d584f60a0de113
Serj Sidelnikov <serj.v.sidelnikov@gmail.com>
2020-04-16 16:35:35 +0300

    Markup page cart

---
+++ routes/card.js
@@ -0,0 +1,23 @@
+const {Router} = require('express');
+const Card = require('../models/card');
+const Courses = require('../models/courses');
+
+const router = Router();
+
+router.post('/add', async (req, res) => {
+  const course = await Courses.getById(req.body.id);
+  await Card.add(course);
+
+  res.redirect('/card');
+});
+
+router.get('/', async (req, res) => {
+  const card = await Card.fetch();
+
+  res.render('card', {
+    title: 'Корзина',
+    card,
+  });
+});
+
+module.exports = router;
