aed993dca8a59356859c66e18c75fc5553ca567f
Serj <sv@spiks.ru>
2020-04-17 10:39:18 +0300

    Dynamic change cart

--- public/app.js
+++ public/app.js
@@ -1,7 +1,48 @@
+const toCurrency = (price) => {
+  return new Intl.NumberFormat('ru-Ru', {
+    currency: 'rub',
+    style: 'currency',
+  }).format(price);
+};
+
 document.querySelectorAll('.price')
   .forEach((node) => {
-    node.textContent = new Intl.NumberFormat('ru-Ru', {
-      currency: 'rub',
-      style: 'currency',
-    }).format(node.textContent);
+    node.textContent = toCurrency(node.textContent);
   });
+
+const cart = document.querySelector('#cart');
+
+if (cart) {
+  cart.addEventListener('click', (event) => {
+    if (event.target.classList.contains('js-remove')) {
+      const id = event.target.dataset.id;
+
+      fetch(`/card/remove/${id}`, {
+        method: 'delete',
+      }).then((res) => res.json())
+        .then((card) => {
+          if (card.courses.length) {
+            const html = card.courses.map((c) => {
+              return `
+                <tr>
+                  <td>${c.title}</td>
+                  <td>${c.count}</td>
+                  <td>
+                    <button
+                      class="btn btn-small js-remove"
+                      data-id="${c.id}"
+                    >Удалить</button>
+                  </td>
+                </tr>
+              `;
+            }).join('');
+
+            cart.querySelector('tbody').innerHTML = html;
+            cart.querySelector('.price').textContent = toCurrency(card.price);
+          } else {
+            cart.innerHTML = '<p>Корзина пуста</p>';
+          }
+        });
+    }
+  });
+}
