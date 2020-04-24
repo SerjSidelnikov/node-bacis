ce5de7744ea09b6cda7496470e39c55971e73cc3
Serj <sv@spiks.ru>
2020-04-24 15:43:48 +0300

    Send mail

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ const ordersRoutes = require('./routes/orders');
 const authRoutes = require('./routes/auth');
 const varMiddleware = require('./middleware/variables');
 const userMiddleware = require('./middleware/user');
-
-const MONGODB_URI = `mongodb+srv://Serj:tPeH3KW428xz5cl6@cluster0-mo7px.mongodb.net/shop?retryWrites=true&w=majority`;
+const keys = require('./keys');
 
 const app = express();
 
@@ -30,7 +29,7 @@ const hbs = exphbs.create({
 
 const store = new MongoStore({
   collection: 'sessions',
-  uri: MONGODB_URI,
+  uri: keys.MONGODB_URI,
 });
 
 app.engine('hbs', hbs.engine);
@@ -40,7 +39,7 @@ app.set('views', 'views');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
-  secret: 'some secret value',
+  secret: keys.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
   store,
@@ -61,7 +60,7 @@ const PORT = process.env.PORT || 3000;
 
 async function start() {
   try {
-    await mongoose.connect(MONGODB_URI, {
+    await mongoose.connect(keys.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,17 @@
 const {Router} = require('express');
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgrid = require('nodemailer-sendgrid-transport');
 
 const User = require('../models/user');
+const keys = require('../keys');
+const regEmail = require('../emails/registration');
 
 const router = Router();
+
+const transporter = nodemailer.createTransport(sendgrid({
+  auth: {api_key: keys.SENDGRID_API_KEY},
+}));
 
 router.get('/login', async (req, res) => {
   res.render('auth/login', {
@@ -69,6 +77,7 @@ router.post('/register', async (req, res) => {
       });
 
       await user.save();
+      await transporter.sendMail(regEmail(email));
       res.redirect('/auth/login#login');
     }
   } catch (e) {
