a5edc226fee4acbbdb4353cfafc4666c1e29a0c3
Сергей Сидельников <serj2107@gmail.com>
2020-04-27 20:55:56 +0300

    Delete keys

--- keys/keys.dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  MONGODB_URI: 'mongodb+srv://Serj:tPeH3KW428xz5cl6@cluster0-mo7px.mongodb.net/shop?retryWrites=true&w=majority',
-  SESSION_SECRET: 'some secret value',
-  SENDGRID_API_KEY: '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',
-  EMAIL_FROM: 'sv@spiks.ru',
-  BASE_URL: 'http://localhost:3000',
-};
